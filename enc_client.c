b2bc5043424e3f139b55ce614ad43283e2dfda7c
AlanGacihi <alanmamicha@gmail.com>
2023-08-16 13:27:09 +0300

    Add

--- enc_client.c
+++ enc_client.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Send combined data to enc_server
-    send(sockfd, combined_data, strlen(combined_data), 0);
+    //send(sockfd, combined_data, strlen(combined_data), 0);
 
     //Receive ciphertext from enc_server
     char ciphertext[BUFFER_SIZE];
